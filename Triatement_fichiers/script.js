a8043209c3962aa6caadccbd9e292526e2f0211b
SoumiaSAADOUN <es_saadoun@esi.dz>
2022-08-31 11:49:58 +0100

    edi final

--- Triatement_fichiers/script.js
+++ Triatement_fichiers/script.js
@@ -1,14 +1,51 @@
+// On récupère le fichier
 let inputFile= document.getElementById('inputFile');
 inputFile.addEventListener('change', ()=>{
+    inputFile.setAttribute("disabled", true);
     var reader= new FileReader();
-    
+
+    // liste des critères de blockage 
+    let keyWords=['CLOTURE', 'INEXISTANT', 'RETRAITS A VUE SERVICE'];
+
     // let valides=[];
-    let non_valides=[];
+    let non_valides=[]; //  la liste qui va contenir les lignes traitées
+
     reader.onload = ()=>{
-        let lines = reader.result.split('\n');
+        
+        /* PROGRESS BAR */
+        let bar = document.getElementById('theBar');
+        bar.style.visibility='visible';
+        let width= 1;
+        let id = setInterval(frame,10)
+        function frame(){
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+                document.getElementById('success').style.visibility='visible';
+              } else {
+                width++;
+                bar.style.width = width + "%";
+                bar.innerHTML = width + "%";
+              }
+        }
+
+
+        /* TRAITEMENT DES LIGNES */
+        /*
+         on met le contenu du fichier lu dans une liste, 
+         comme chaque ligne contient un enregidtrement complet
+         on considère le sot de ligne comme séparateur
+        */
+       
+        let lines = reader.result.split('\n'); 
+
         lines.forEach((line)=>{
-            if (line.includes('CLOTURE', 'INEXISTANT', 'EN INSTANCE DE CLOTURE')){
-                non_valides.push(line);
+            // pour chaque ligne, on verifie si elle est marquée par un des critères de blocage
+            if (keyWords.some((word)=>line.includes(word))){
+                let newLine = line.replace('*',' ');               
+                newLine = newLine.replaceAll('*',';');             
+                non_valides.push(newLine);
+                
             }
             // else{
             //     valides.push(line);
@@ -20,7 +57,7 @@ inputFile.addEventListener('change', ()=>{
         let download_non_valides = document.createElement("a");
         download_non_valides.href=URL.createObjectURL(non_valides_file) ;
         download_non_valides.target='_blank';
-        download_non_valides.download=`non_valides_${(new Date().toJSON().slice(0,10))}.txt`;
+         download_non_valides.download=`non_valides_${(new Date().toJSON().slice(0,10))}.csv`;
         download_non_valides.click();
 
 
