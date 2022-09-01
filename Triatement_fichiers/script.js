726a37667e6fbeaad41a70fc7fd61cec6979573e
SoumiaSAADOUN <es_saadoun@esi.dz>
2022-09-01 15:55:26 +0100

    separate files

--- Triatement_fichiers/script.js
+++ Triatement_fichiers/script.js
@@ -6,8 +6,9 @@ inputFile.addEventListener('change', ()=>{
 
     // liste des critères de blockage 
     let keyWords=['CLOTURE', 'INEXISTANT', 'RETRAITS A VUE SERVICE'];
+   
 
-    // let valides=[];
+     let valides=[];
     let non_valides=[]; //  la liste qui va contenir les lignes traitées
 
     reader.onload = ()=>{
@@ -33,32 +34,61 @@ inputFile.addEventListener('change', ()=>{
         /* TRAITEMENT DES LIGNES */
         /*
          on met le contenu du fichier lu dans une liste, 
-         comme chaque ligne contient un enregidtrement complet
+         comme chaque ligne contient un enregisrement complet
          on considère le sot de ligne comme séparateur
         */
        
         let lines = reader.result.split('\n'); 
+		let total=0;
 
-        lines.forEach((line)=>{
+        for (let i=166; i<lines.length; i++)
+
+        // lines.forEach((line)=>
+        {
+            let line= lines[i];
             // pour chaque ligne, on verifie si elle est marquée par un des critères de blocage
             if (keyWords.some((word)=>line.includes(word))){
                 let newLine = line.replace('*',' ');               
                 newLine = newLine.replaceAll('*',';');             
                 non_valides.push(newLine);
+				total++;
                 
+            }
+            else if(line.includes('0,00')){
+                valides.push(line); total++;
             }
             // else{
             //     valides.push(line);
             // }
-        })
+        }
        
+        document.getElementById("total").innerText=total;
+        document.getElementById("bloques").innerText=non_valides.length;
+        document.getElementById("valides").innerText=valides.length;
+        document.getElementById("result").style.visibility="visible";
      
+	    console.log(`Nombre d'enregistrements dans l'EDI TEST ${total}`);
+        console.log(`Nombre d'enregistrements A BLOQUER dans l'EDI TEST  ${non_valides.length}`);
+        console.log(`Nombre d'enregistrements VALIDES dans l'EDI TEST  ${valides.length}`);
+	 
         let non_valides_file= new Blob([non_valides.join('\n')], {type: 'data:text/txt;charset=utf-8,'});
         let download_non_valides = document.createElement("a");
         download_non_valides.href=URL.createObjectURL(non_valides_file) ;
         download_non_valides.target='_blank';
-         download_non_valides.download=`non_valides_${(new Date().toJSON().slice(0,10))}.csv`;
+        download_non_valides.download=`non_valides_${(new Date().toJSON().slice(0,10))}.csv`;
         download_non_valides.click();
+       
+        let current =0;      
+       
+        while(current < valides.length){
+            let batch = valides.slice(current, current+800000);
+            current+= 800000;
+            let valides_file= new Blob([batch.join('\n')], {type:'text/plain'});
+            let download_valides = document.createElement("a");
+            download_valides.href=URL.createObjectURL(valides_file);
+            download_valides.download=`valides_${current}_${(new Date().toJSON().slice(0,10))}.txt`;
+            download_valides.click();
+        }
 
 
 
@@ -72,3 +102,7 @@ inputFile.addEventListener('change', ()=>{
     reader.readAsText(inputFile.files[0]);
    
 })
+
+let inputFinalEDI= document.getElementById('inputFinalEDI');
+
+
